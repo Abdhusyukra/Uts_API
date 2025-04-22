dbf6ea08870be6ec4481d1a5d200d518b95a0305
Abdhusyukra <abdhusyukra02@gmail.com>
2025-04-22 18:57:40 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\Api\MapController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/game', [MapController::class, 'getAllTeams']);
+
+Route::get('/', [MapController::class, 'getAllTeams']);
 
 
